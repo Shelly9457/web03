e4e354f4aa066fa36da7f1c36d7a3e144a49f0c7
Shelly9457 <96901490+Shelly9457@users.noreply.github.com>
2023-01-13 10:16:31 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function menu() {
+    const threeline = document.querySelector('.threeline')
+
+    threeline.classList.toggle('active')
+
+    const navber_hidden = document.querySelector('.navber__links--hidden')
+
+    navber_hidden.classList.toggle('active')
+
+}
